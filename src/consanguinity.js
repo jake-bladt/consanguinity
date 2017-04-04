3d8285f696c682e4714bcf5de233d8ef3cb56056
Jake Bladt <jake@jakebladt.com>
2017-04-04 19:53:05 -0400

    +subject

--- src/consanguinity.js
+++ src/consanguinity.js
@@ -1,4 +1,12 @@
+let OUTSIDE_PARENT = "arbitrary value";
+
+let Subject = (parameters) => {
+  return {
+    father: parameters.father || OUTSIDE_PARENT,
+    mother: parameters.mother || OUTSIDE_PARENT,
+    consanguinityWith: (subject) => 0
+  }
+}
 
 
-
-module.exports = {};
+module.exports = { OUTSIDE_PARENT, Subject };
